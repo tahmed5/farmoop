19f17cb4ea4d6a052adb412f4a756c77f8a99b41
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2017-09-21 21:07:27 +0100

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,3 +1,5 @@
+import random
+
 class Crop:
     """A generic food crop"""
 
@@ -22,11 +24,41 @@ class Crop:
     def report(self):
         #return a dictionary containing the type, status, growth and days growing
         return {'type':self._type,'status':self._status,'growth':self._growth,'days growing':self._days_growing}
-        
+    
+    def update_status(self):
+        if self._growth > 15:
+            self._status = "Old"
+        elif self._growth > 10:
+            self._status = "Mature"
+        elif self._growth > 5:
+            self._status = "Young"
+        elif self._growth > 0:
+            self._status = "Seedling"
+        elif self._growth == 0:
+            self._status = "Seed"
+    
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth += self._growth_rate
+            #increment number of days growing
+            self._days_growing += 1
+            #update the status of crop
+            self.update_status()
+            
+def auto_grow(crop, days):
+    #grow the crop
+    for day in range(days):
+        light = random.randint(1,10)
+        water = random.randint(1,10)
+        crop.grow(light,water)
+
+
 def main():
     #intialise the class
     new_crop = Crop(1,4,3)
     print(new_crop.needs())
+    print(new_crop.report())
+    auto_grow(new_crop,30)
     print(new_crop.report())
     
 if __name__ == "__main__":
