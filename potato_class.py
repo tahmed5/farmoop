1a82e953e12337127f5d69020ab8712bb243164b
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2017-09-22 10:35:03 +0100

    Add files via upload

---
+++ potato_class.py
@@ -0,0 +1,38 @@
+from crop import *
+
+class Potato(Crop):
+    """A potato crop"""
+
+    #constructor
+    def __init__(self):
+        #call the parent class constructor with default values for potato
+        # growth rate = 1; light need = 3; water need = 6
+        super().__init__(1,3,6)
+        self._type = "Potato"
+
+    #override grow method for subclass
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            if self._status == "Seedling" and water > self._water_need:
+                self._growth += self._growth_rate * 1.5
+            elif self._status == "Young" and water > self._water_need:
+                self._growth += self._growth_rate * 1.25
+            else:
+                self._growth += self._growth_rate
+        #increment day growing
+        self._days_growing += 1
+        #update the status
+        self.update_status()
+
+def main():
+    #create a new potato crop
+    potato_crop = Potato()
+    print(potato_crop.report())
+    #manually grow the crop
+    manual_grow(potato_crop)
+    print(potato_crop.report())
+    manual_grow(potato_crop)
+    print(potato_crop.report())
+
+if __name__ == "__main__":
+    main()
