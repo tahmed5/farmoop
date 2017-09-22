f0667bd9ae0ba11a881f5b2ddc86c26afaedbe11
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2017-09-22 10:35:29 +0100

    Update crop.py

--- crop.py
+++ crop.py
@@ -51,6 +51,30 @@ def auto_grow(crop, days):
         light = random.randint(1,10)
         water = random.randint(1,10)
         crop.grow(light,water)
+def manual_grow(crop):
+    #get the light and water values from the user
+    valid = False
+    while not valid:
+        try:
+            light = int(input('Please enter a light value (1-10):'))
+            if 1 <= light <= 10:
+                valid = True
+            else:
+                print('Value entered not valid - please enter a value between 1 and 10')
+        except ValueError:
+            print('Value entered not valid - please enter a value between 1 and 10')
+    valid = False
+    while not valid:
+        try:
+            water = int(input('Please enter a water value (1-10):'))
+            if 1 <= water <= 10:
+                valid = True
+            else:
+                 print('Value entered not valid - please enter a value between 1 and 10')
+        except ValueError:
+            print('Value entered not valid - please enter a value between 1 and 10')
+    #grow crop
+    crop.grow(light,water)
 
 
 def main():
@@ -58,7 +82,7 @@ def main():
     new_crop = Crop(1,4,3)
     print(new_crop.needs())
     print(new_crop.report())
-    auto_grow(new_crop,30)
+    manual_grow(new_crop)
     print(new_crop.report())
     
 if __name__ == "__main__":
