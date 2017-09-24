4f84bc861c2c1de4cc668d0b8914e34e17617374
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2017-09-24 19:34:55 +0100

    Update crop.py

--- crop.py
+++ crop.py
@@ -76,14 +76,53 @@ def manual_grow(crop):
     #grow crop
     crop.grow(light,water)
 
+def display_menu():
+    print('1. Grow manually over 1 day')
+    print('2. Grow automatically over 30 days')
+    print('3. Report status')
+    print('0. Exist test program')
+    print()
+    print('Please select an option from the above menu')
+    
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input('Option Selected: '))
+            if 0 <= choice <= 4:
+                option_valid = True
+            else:
+                print('Please enter a valid option')
+        except ValueError:
+            print('Please enter a valid option')
+    return choice
+ 
+def manage_crop(crop):
+    print('This is the crop management program')
+    print()
+    noexit = True
+    while noexit:
+        display_menu()
+        option = get_menu_choice()
+        print()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        elif option == 2:
+            auto_grow(crop,30)
+            print()
+        elif option == 3:
+            print(crop.report())
+            print()
+        elif option == 0:
+            noexit = False
+            print()
+    print('Thank you for using the crop management program')
 
 def main():
     #intialise the class
-    new_crop = Crop(1,4,3)
-    print(new_crop.needs())
-    print(new_crop.report())
-    manual_grow(new_crop)
-    print(new_crop.report())
+    new_crop = Crop(1, 4, 3)
+    manage_crop(new_crop)
     
 if __name__ == "__main__":
     main()
